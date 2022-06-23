2a4299b8362ce1420d6de1cb30742be733f53d8f
Zikoi5 <gandgziko@gmail.com>
2022-06-23 23:29:10 +0500

    User checker yana ishladi

--- mongo/index.js
+++ mongo/index.js
@@ -1,26 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
+// const express = require("express");
+const mongoose = require("mongoose")
 // const authRouter = require("@/authRouter");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
 
-const PORT = process.env.MONGO_PORT || 5001;
-const app = express();
+// const PORT = process.env.MONGO_PORT || 5001;
+// const app = express();
 
-app.use(cors());
+// app.use(cors());
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_CONNECT_URL);
-    app.listen(PORT, () => {
-      console.log(`Mongodb listening port ${PORT}`);
-    });
+    await mongoose.connect(process.env.MONGO_CONNECT_URL)
+    console.log(`Mongodb started`)
+    // app.listen(PORT, () => {
+    //   console.log(`Mongodb listening port ${PORT}`);
+    // });
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-};
+}
 
 module.exports = start
